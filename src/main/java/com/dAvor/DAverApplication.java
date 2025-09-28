a20bc13309782b2c2417a1966d7f7c5648515347
gptdevansh <gptdevansh@gmail.com>
2025-09-28 15:30:29 +0530

    Project Initialisation

---
+++ src/main/java/com/dAvor/DAverApplication.java
@@ -0,0 +1,13 @@
+package com.dAvor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DAverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DAverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dAvor/DAverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dAvor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DAverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
