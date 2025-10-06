0b37b6f60cfd61c0f04d37686cd41866e6c0acee
gptdevansh <gptdevansh@gmail.com>
2025-10-07 03:48:36 +0530

    Made app production-grade with validation and global exception handling

--- src/main/java/com/dAvor/exception/ResourceNotFoundException.java
+++ src/main/java/com/dAvor/exception/ResourceNotFoundException.java
@@ -1,9 +1,13 @@
 package com.dAvor.exception;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 
 @Getter
+@NoArgsConstructor
+
 public class ResourceNotFoundException extends RuntimeException {
     private String resourceName;
     private String fieldName;
@@ -16,9 +20,6 @@ public class ResourceNotFoundException extends RuntimeException {
 */
 
     private Object fieldValue;
-
-    public ResourceNotFoundException() {
-    }
 
 
     public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
